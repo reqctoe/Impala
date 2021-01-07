622ffe256dd11fc5ed298c9daef78c2345547875
JorisOud <jorisoud@hotmail.com>
2021-01-07 14:40:14 +0100

    load tiles af

--- code/classes/tile.py
+++ code/classes/tile.py
@@ -1,5 +1,6 @@
 
-
+# DIT MOET WORDEN GEINPORT VOOR DE GAME CLASS
+from csv import DictReader
 
 class Tile:
     """This class is used to create tiles for the Rush Hour board.
@@ -7,10 +8,10 @@ class Tile:
       counting from the top and col from the left."""
 
 
-    def __init__(self, row, col, id):
+    def __init__(self, id, row, col):
+        self.id = id
         self.row = row
         self.col = col
-        self.id = id
         self.ocupied = False
     
     def set_occupied(self):
@@ -23,6 +24,31 @@ class Tile:
 
         self.occupied = False
 
-    def load_tiles(self, data):
+
+
+    # DEZE IS VOOR IN DE GAME CLASS
+    def load_tiles(self, data, board_size):
 
         self.tiles = {}
+        
+        # add all tiles with occupied to false
+        for row in board_size:
+            for col in board_size:
+                id = ((row - 1) * board_size) + col
+                new_tile = Tile(id, row, col)
+                self.tiles[new_tile.id] = new_tile
+
+        # set tiles that have a car on them as occupied
+        with open(FILE, "r") as data: # HIER EVEN DE GOEIE FILENAAM TOEVOEGEN IN DE GAME CLASS
+            data_dict = DictReader(data)
+
+        for car in data_dict:
+            # set first tile to occupied
+            id = ((car[row] - 1) * board_size) + car[col]   # hier wordt ervan uitgegaan dat de header van de file klopt
+            tile = self.tiles[id]
+            tile.occupied = True
+
+            # TODO set second tile to occupied
+
+            # TODO if length is 3 set 3rd tile to occupied
+
