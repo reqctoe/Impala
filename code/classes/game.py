e616e278f4575aebeb9ee2e605f38733e9636062
iza98 <70637692+iza98@users.noreply.github.com>
2021-01-07 17:30:07 +0100

    Game class geupdate
    won, valid_move, en move aangepast

--- code/classes/game.py
+++ code/classes/game.py
@@ -1,31 +1,39 @@
 from tile import Tile
 from car import Car
 
-class Game:
-    def __init__(self, dimension, board_file):
+from math import ceil
 
-        self.dimension = dimension
+class Game():
+    def __init__(self, board_size, board_file):
 
-        # TODO: inladen auto's en tiles
+        self.board_size = board_size
+
+        # tiles is a dictionary that maps a tile number to the corresponding tile object
         self.tiles = {}
+        # cars is a dictionary that maps a car letter to the corresponding car object
         self.cars = {}
 
-        self.load_tiles(dimension)
+        # load cars and tiles
+        self.load_tiles()
         self.load_cars(board_file)
 
-    def load_tiles(self, board_size):
+        # set winning tile
+        win_tile = (ceil(self.board_size / 2) - 1) * self.board_size + self.board_size
+        self.winning_tile = self.tiles[win_tile]
+
+    def load_tiles(self):
         """
             Load tiles
         """
 
         # add all tiles with occupied to false
-        for row in board_size:
-            for col in board_size:
-                id = ((row - 1) * board_size) + col
+        for row in self.board_size:
+            for col in self.board_size:
+                id = ((row - 1) * self.board_size) + col
                 new_tile = Tile(id, row, col)
                 self.tiles[new_tile.id] = new_tile
 
-    def load_cars(self, board_file, dimension):
+    def load_cars(self, board_file):
         """
         Load cars from file
         """
@@ -37,51 +45,59 @@ class Game:
     def current_board(self):
         pass
 
-    # TODO: aanpassen
-    def valid_move(self, car, destination):
+    def valid_move(self, car_id, move):
         """
-        Checks if the car is moved within it's row or column
+        Checks if the move is valid
         """
-        # if the orenientation of the car is horizontal, check if the move is in the same row
+        car = self.cars[car_id]
+
+        # check if you don't move through any cars or walls, return false if you do
+        # if the car's orientation is horizontal, only look through its row 
         if car.orientation == 'H':
-            return 
-        # if the orientation is vertical, check if the move is in the same column
-        elif car.orientation == 'V':
-        
-        # check of beweging mogelijk is (hij gaat niet door andere auto's of muren heen)
-        # return false als dat wel zo is 
-        Voor elke tegel die gepasseerd wordt:
-            als de tegel occupied is:
+            # check if you don't move through walls
+            if 1 > car.row + move >= self.board_size:
                 return False
-        als destination of destination+1 niet op bord:
-            return false
 
+            # TODO: ook werkend krijgen voor negatieve bewegingen
+            # check if you don't move through cars
+            for i in range(1, move+1):
+                if self.tiles[((car.row - 1) * self.board_size) + i].occupied:
+                    return False
+        # if it is vertical, only look through its column
+        else:
+            if 1 > car.col + move > self.board_size:
+                return False
+            for i in range(1, move+1):
+                if self.tiles[((i - 1) * self.board_size) + car.col].occupied == False:
+                    return False
+        
+        return True
 
-    def move(self, car, destination):
+        # # if the orenientation of the car is horizontal, check if the move is in the same row
+        # if car.orientation == 'H' & :
+        #     return 
+        # # if the orientation is vertical, check if the move is in the same column
+        # elif car.orientation == 'V':
+
+    def move(self, car, move):
         """
-        Move a car to a different possition, if possible
+        Move a car to a different possition
         """
-
-        # if the move is valid, change the position of the car
         # set current tiles to unoccupied
         for tile in self.cars[car.id].tiles:
             tile.set_unoccupied()
 
         # set new position car 
-        # TODO: method uit Car class
-        self.cars[car.id].row = destination.row
-        self.cars[car.id].col = destination.col
+        self.cars[car.id].update_position(move)
 
         # set tiles to occupied 
         for tile in self.cars[car.id].tiles:
             tile.set_occupied()
-        
-        return True
 
     def game_won(self):
         """
         Checks if the red car (with id X) is in front of the exit
         """
-        als auto X op row == dimension-1 & col = dim/2(even) of col= dim/2+1(oneven) staat
+        if self.winning_tile in self.cars['X'].tiles:
             return True
-        return False  
+        return False
