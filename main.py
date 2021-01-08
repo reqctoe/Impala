aec3865787391de1ff9b1313586916a1d37fa412
JorisOud <jorisoud@hotmail.com>
2021-01-08 12:27:22 +0100

    added board file formatstring to game

--- main.py
+++ main.py
@@ -25,10 +25,6 @@ if __name__ == "__main__":
     game_number, board_dimension = argv[1], argv[2]
     game = Game(board_dimension, game_number)
 
-    #VOOR IN DE GAME CLASS
-    game_file = f"Rushhour{board_dimension}x{board_dimension}_{game_number}.csv"
-    #IN DE GAME CLASS MOET DUS OOK NOG EVEN DE FILE GEOPEND WORDEN
-
     print("Enter a move in the folowing format:\n[car_id],[move]\n" +
             "when you want to move left or up, enter a negative number.")
     
