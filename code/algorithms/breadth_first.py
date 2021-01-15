67fe50a90b356b4554d54169566f6cf3f0c84ec4
reqctoe <70585516+reqctoe@users.noreply.github.com>
2021-01-15 17:49:57 +0100

    poging tot fixen

--- code/algorithms/breadth_first.py
+++ code/algorithms/breadth_first.py
@@ -11,8 +11,11 @@ class BreadthFirst:
         self.state_keys = [self.game.give_board()]
 
         self.best_solution = None
+        self.queue = int(0)
 
         self.create_solution()
+
+
 
     def get_car_coords(self, game_state):
         """
@@ -24,44 +27,50 @@ class BreadthFirst:
             info.append(f"{car.id},{car.orientation},{car.col},{car.row},{car.length}")
 
         moves = game_state.get_moves()
-
+        # print([info, moves])
         return [info, moves]        
 
 
     def get_next_state(self):
         # return self.states.pop(0)
         key = self.state_keys.pop(0)
+        
+        print(f"Queue length: {len(self.state_keys)}")
+        
         return self.states[key]
 
-    def build_children(self, game_state):
+    def build_children(self, state_data):
         """
         Creates all possible child-states and adds them to the list of states
         """
-        game = Game(self.game.board_size, self.game.game_number, game_state)
+        game_node = Game(self.game.board_size, self.game.game_number, state_data)
 
         # 
-        for car in game.car_ids: 
-            for i in range(- game.board_size - 2, game.board_size - 1):
+        for car in game_node.car_ids: 
+            for i in range(- game_node.board_size - 2, game_node.board_size - 1):
                 if i != 0:
                     # check if the move is valid/possible
-                    if game.valid_move(car, i):
-                        if game.get_moves() and [car,-i] == game.get_moves()[-1]:
+                    if game_node.valid_move(car, i):
+                        if game_node.get_moves() and [car,-i] == game_node.get_moves()[-1]:
                             break
                         # make a new state
-                        # new_game_state = copy.deepcopy(game_state)
+                        new_game_state = Game(self.game.board_size, self.game.game_number, state_data)
                         # move the car
-                        # new_game_state.move(car, i)
-                        game.move(car, i)
+                        new_game_state.move(car, i)
 
                         # check if it is a solution
-                        if game.game_won():
-                            print("heuy")
-                            self.best_solution = game.get_moves()
+                        if new_game_state.game_won():
+                            self.best_solution = new_game_state.get_moves()
+                            # print(self.best_solution)
+                            print(len(self.best_solution))
                         else:
-                            if game.give_board() not in self.states:
-                                print("waaah")
-                                self.state_keys.append(game.give_board())
-                                self.states[game.give_board()] = self.get_car_coords(game)
+                            if new_game_state.give_board() not in self.states:
+                                self.queue += 1
+                                print(f"Added to queue: {self.queue}")
+                                print(f"Depth: {len(new_game_state.get_moves())}")
+                                self.state_keys.append(new_game_state.give_board())
+                                self.states[new_game_state.give_board()] = self.get_car_coords(new_game_state)
+
             
                         # if new_game_state.game_won():
                         #     self.best_solution = new_game_state.get_moves()
@@ -88,14 +97,13 @@ class BreadthFirst:
         while self.state_keys:
         # while self.states:
             count += 1
-            new_state = self.get_next_state()
-            if count % 1000 == 0:
-                print(len(new_state.get_moves()))
+            new_state_data = self.get_next_state()
+            if count % 5 == 0:
+                print(f"ROW:{len(new_state_data[1])}")
 
-            self.build_children(new_state)
+            self.build_children(new_state_data)
 
             if self.best_solution != None:
-                print("hoi")
                 break
 
     def get_command(self):
