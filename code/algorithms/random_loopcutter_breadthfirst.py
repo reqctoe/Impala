1e194843d3cf6dab928b317a3b3bc422e0cf67e8
JorisOud <jorisoud@hotmail.com>
2021-01-19 14:54:51 +0100

    finished loop cutter

--- code/algorithms/random_loopcutter_breadthfirst.py
+++ code/algorithms/random_loopcutter_breadthfirst.py
@@ -7,40 +7,20 @@ from code.algorithms.random_repeater import Random_repeater
 
 class Random_repeater_adjusted(Random_repeater):
 
-    def __init__(self, board_size, game_number):
-        self.board_size = int(board_size)
-        self.game_number = game_number
-        self.board_file = f"data/gameboards/Rushhour{board_size}x{board_size}_{game_number}.csv"
-        
-        self.best_solution = []
-        self.best_solutions = []
-
-        for i in range(10000):
-            self.game = Game(self.board_size, self.game_number)
-            new_solution = self.create_solution()
-            if new_solution:
-                print(len(new_solution))
-                self.best_solution = new_solution
-                self.best_solutions.append(new_solution)
-
-    def give_solutions(self):
-        return self.best_solutions
-
-
+    def give_solution(self):
+        return self.best_solution
 
 
 class Random_loopcutter_breadthfirst:
     def __init__(self, game):
         self.game = deepcopy(game)
 
-        # select the 10 best solutions out of 1000 tries
+        # get best solutions out of 1000 random tries using Random_repeater
         self.random_repeater = Random_repeater_adjusted(self.game.board_size, self.game.game_number)
-        self.solutions = self.random_repeater.give_solutions()
-        self.solutions = self.solutions[-11:-1]
-        self.cut_solutions = []
+        self.solution = self.random_repeater.give_solution()
 
-        for solution in self.solutions:
-            self.cut_solutions.append(self.loopcutter(solution))
+        # cut loops of every solution an keep save shortest result
+        self.cut_solution = self.loopcutter(self.solution)
 
     def loopcutter(self, solution):
         game = deepcopy(self.game)
@@ -48,4 +28,30 @@ class Random_loopcutter_breadthfirst:
         game_boards.append(game.give_board())
 
         for move in solution:
-            game.move()
+            game.move(*move)
+            game_boards.append(game.give_board())
+
+        # keep cutting until there are no more double states left
+        while True:
+            game_boards_set = set(game_boards)
+
+            for board in game_boards_set:
+                # get all indices of a game state in board_list
+                board_indices = [i for i, e in enumerate(game_boards) if e == board]
+
+                # if a state occurs more than once, delete the moves in between
+                if len(board_indices) > 1:
+                    del game_boards[board_indices[0]: board_indices[-1]]
+                    print(f"Removed move {board_indices[0]} to {board_indices[-1]}")
+                    del solution[board_indices[0]: board_indices[-1]]
+
+                    break
+            else:
+                break
+
+        return solution
+
+    def get_command(self):
+        command_list = self.cut_solution.pop(0)
+        car, move = command_list[0:2]
+        return f"{car},{move}"
