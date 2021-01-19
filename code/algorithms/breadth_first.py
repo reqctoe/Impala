f02e1d5660ae1b0d86a1b0915565fc7f41827cab
reqctoe <70585516+reqctoe@users.noreply.github.com>
2021-01-19 10:28:01 +0100

    some results

--- code/algorithms/breadth_first.py
+++ code/algorithms/breadth_first.py
@@ -7,11 +7,15 @@ class BreadthFirst:
     
     def __init__(self, game):
         self.game = deepcopy(game)
+        # self.clean = []
+        self.row = 1
 
         # initiate archive
         self.state_keys = [self.game.give_board()]
-        self.states = {}
-        self.add_to_archive(game)
+        self.row1 = {}
+        self.row2 = {}
+        self.row2[self.game.give_board()] = self.get_node_data(self.game)
+        self.current_row = {}
         
         # game variables
         self.move_range = list(range(- self.game.board_size + 2, self.game.board_size - 1))
@@ -24,9 +28,39 @@ class BreadthFirst:
 
 
     def add_to_archive(self, game):
-        if game.give_board() not in self.states:
+        if game.give_board() not in self.row1 or self.row2 or self.current_row:
             self.state_keys.append(game.give_board())
             self.states[game.give_board()] = self.get_node_data(game)
+            # self.order_archive(game)
+
+    # def order_archive(self, game):
+    #     row = len(game.get_moves())
+    #     if row > self.row:
+    #         self.row = row
+    #         self.row1.clear() 
+    #         self.row1 = deepcopy(self.row2)
+    #         self.row2.clear()
+    #         self.row2 = deepcopy(self.current_row)
+    #         self.current_row.clear()
+        
+    #     self.current_row[game.give_board()] = self.get_node_data(game)
+        
+
+    # def clean_archive(self, game):
+    #     self.clean.append(game.give_board())
+        
+    #     if len(game.get_moves()) > 3:
+    #         for i in ......:
+    #             board = self.clean.pop(i)
+    #             self.states.pop(board)
+
+        # self.clean.append([game.give_board(), self.get_node_data(game)])
+
+        # if len(game.get_moves()) > 3:
+        #     [self.clean.pop(i) for i in .....]
+        #     self.states.clear() 
+        #     for node in self.clean:
+        #         self.states[node[0]] = node[1]
 
 
     def create_solution(self): 
@@ -36,11 +70,15 @@ class BreadthFirst:
         count = 0
 
         while self.state_keys:
-            # count += 1
+            count += 1    
+            # print(self.row1)
+            print(self.row2)
+            # print(self.current_row)
             new_state_data = self.get_next_state()
             game_node = Game(self.game.board_size, self.game.game_number, new_state_data)
             
-            # if count % 5 == 0:
+            if count % 50 == 0:
+                print("beep")
             #     print(f"ROW:{len(new_state_data[1])}")
 
             for car in self.cars: 
@@ -49,6 +87,7 @@ class BreadthFirst:
                     if game_node.valid_move(car, i):
                         if game_node.get_moves() and [car,-i] == game_node.get_moves()[-1]:
                             break
+                        
                         self.build_child(game_node, car, i)
 
                 if self.best_solution != None:
@@ -58,9 +97,13 @@ class BreadthFirst:
     def get_next_state(self):
         key = self.state_keys.pop(0)
         
-        print(f"Queue length: {len(self.state_keys)}")
-        state_data = self.states[key]
-        self.states[key] = ""
+        # print(f"Queue length: {len(self.state_keys)}")
+        if key not in self.row2:
+            state_data = self.current_row[key]
+            self.current_row[key] = ""
+        else:
+            state_data = self.row2[key]
+            self.row2[key] = ""
 
         return state_data
 
