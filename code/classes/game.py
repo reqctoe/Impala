96dc64ecd7ab33c94d3b3e3237acb4efc0c1715b
Eline van de Lagemaat <elinevdla@gmail.com>
2021-01-26 17:48:42 +0100

    Eline opschonen

--- code/classes/game.py
+++ code/classes/game.py
@@ -7,22 +7,23 @@ class Game():
     
     def __init__(self, board_size, game_number, data = None):
         
+        # game constants
         self.board_size = int(board_size)
         self.game_number = int(game_number)
-        # winning tile id        
         self.winning_tile = ceil(self.board_size / 2) * self.board_size
+       
         # board occupation list
         self.board = []
+        
         # map tile numbers to occupying car
-        self.tile_occupation = {}
-        # map tile numbers to the corresponding tile object
         self.tiles = {}
-        # map car letters to the corresponding car object
+        self.tile_occupation = {}
         self.cars = {}
         self.car_ids = []
-
+        
         self.load_tiles()
-        # load cars and tiles from board file
+        
+        # load cars and tiles from board file or provided data
         if not data:
             self.data = data
             self.moves = []
@@ -52,14 +53,14 @@ class Game():
 
     def load_cars(self, board_file):
         """
-        Load cars from file
+        Load cars from provided data or csv file
         """
-
+        # provided data
         if self.data:
             for line in board_file:
                 car_line = line.split(",")
                 self.cars[car_line[0]] = Car(*car_line)
-        # open file and create car objects
+        # csv file
         else:    
             with open(board_file) as f:
                 next(f)
@@ -67,15 +68,15 @@ class Game():
                     car_line = line.split(",")
                     self.cars[car_line[0]] = Car(*car_line)
 
+        # create tile occupancy dictionary and car ID list 
         for car in self.cars:
-            # create list of tiles occupied by car
             self.cars[car].occupies_tiles(self.board_size)
-            self.car_ids.append(self.cars[car].id)
+            self.car_ids.append(self.cars[car].car_attributes()["id"])
             
-            # set tiles to occupied
+            # set tile objects to occupied
             for tile in self.cars[car].tiles:
                 self.tiles[tile].set_occupied()
-    
+
 
     def current_board(self):
         """
@@ -84,11 +85,14 @@ class Game():
         
         # check for occupation and create dictionary of occupied tiles
         for car in self.cars:
-            for tile in self.cars[car].tiles:
-                if len(self.cars[car].id) == 2:
-                    self.tile_occupation[tile] = self.cars[car].id + " "
+            # load car attributes
+            car_info = self.cars[car].car_attributes()
+            
+            for tile in car_info["tiles"]:
+                if len(car_info["id"]) == 2:
+                    self.tile_occupation[tile] = car_info["id"] + " "
                 else:
-                    self.tile_occupation[tile] = self.cars[car].id + "  "
+                    self.tile_occupation[tile] = car_info["id"] + "  "
 
         for tile in self.tiles:
             # check if tile in occupation dictionary
@@ -97,8 +101,9 @@ class Game():
             else:
                 self.board.append("_  ")
 
-            # add newlines 
-            if self.tiles[tile].id % self.board_size == 0:
+            # load tile attributes and add newlines 
+            tile_info = self.tiles[tile].tile_attributes()
+            if tile_info["id"] % self.board_size == 0:
                 self.board.append("\n\n")
 
 
@@ -107,62 +112,62 @@ class Game():
         Checks if the move is valid
         """
         
-        car = self.cars[car_id]
+        car = self.cars[car_id].car_attributes()
         move = int(move)
-        comp = (car.row - 1) * self.board_size
+        comp = (car["row"] - 1) * self.board_size
 
         # check move validity for horizontal car
-        if car.orientation == "H":
+        if car["orientation"] == "H":
             # left wall
-            if car.col + move < 1:
+            if car["col"] + move < 1:
                 return False
             # right wall
-            elif car.col + car.length - 1 + move > self.board_size:
+            elif car["col"] + car["length"] - 1 + move > self.board_size:
                 return False
 
             # car left
             if move == -1:
-                if self.tiles[car.col - 1 + comp].occupied:
+                if self.tiles[car["col"] - 1 + comp].get_occupied():
                     return False
             else:
-                for tile in range(car.col + move + comp, car.col + comp):
-                    if self.tiles[tile].occupied:
+                for tile in range(car["col"] + move + comp, car["col"] + comp):
+                    if self.tiles[tile].get_occupied():
                         return False
            
             # car right
             if move == 1:
-                if self.tiles[car.col + car.length + comp].occupied:
+                if self.tiles[car["col"] + car["length"] + comp].get_occupied():
                     return False
             else:
-                for tile in range(car.col + car.length + comp, car.col + car.length + move + comp):
-                    if self.tiles[tile].occupied:
+                for tile in range(car["col"] + car["length"] + comp, car["col"] + car["length"] + move + comp):
+                    if self.tiles[tile].get_occupied():
                         return False
 
         # check move validity for vertical car      
         else:
             # top wall
-            if car.row + move < 1:
+            if car["row"] + move < 1:
                 return False
             # bottom wall
-            elif car.row + car.length - 1 + move > self.board_size:
+            elif car["row"] + car["length"] - 1 + move > self.board_size:
                 return False
             
             # car top
             if move == -1:
-                if self.tiles[car.col + comp - self.board_size].occupied:
+                if self.tiles[car["col"] + comp - self.board_size].get_occupied():
                     return False
             else:
-                for tile in range(car.col + comp + move * self.board_size, car.col + comp, self.board_size):
-                    if self.tiles[tile].occupied:
+                for tile in range(car["col"] + comp + move * self.board_size, car["col"] + comp, self.board_size):
+                    if self.tiles[tile].get_occupied():
                         return False
             
             # car bottom
             if move == 1:
-                if self.tiles[car.col + comp + car.length * self.board_size].occupied:
+                if self.tiles[car["col"] + comp + car["length"] * self.board_size].get_occupied():
                     return False
             else:
-                for tile in range(car.col + comp + car.length * self.board_size, car.col + comp + (car.length + move) * self.board_size, self.board_size):
-                    if self.tiles[tile].occupied:
+                for tile in range(car["col"] + comp + car["length"] * self.board_size, car["col"] + comp + (car["length"] + move) * self.board_size, self.board_size):
+                    if self.tiles[tile].get_occupied():
                         return False
 
         return True
@@ -174,35 +179,66 @@ class Game():
         """
         
         move = int(move)
+        car_info = self.cars[car_id].car_attributes()
 
         self.moves.append([car_id,move])
 
-        # set current tiles to unoccupied
-        for tile in self.cars[car_id].tiles:
+        # set current tiles to unoccupied and update board string
+        for tile in car_info["tiles"]:
             self.tiles[tile].set_unoccupied()
             self.board[floor((tile - 1) / self.board_size) - 1 + tile] = "_  "
 
         # set new position car 
         self.cars[car_id].update_position(move, self.board_size)
 
-        # set tiles to occupied 
-        for tile in self.cars[car_id].tiles:
+        # set tiles to occupied  and update board string
+        for tile in car_info["tiles"]:
             self.tiles[tile].set_occupied()
 
             if len(car_id) == 2:
                 self.board[floor((tile - 1)/ self.board_size) - 1 + tile] = car_id + " "
             else:
                 self.board[floor((tile - 1)/ self.board_size) - 1 + tile] = car_id + "  "
+    
+
+    def get_game_info(self):
+        """
+        Create dictionary of board size and game number
+        """
+        return {
+            "board_size": self.board_size, 
+            "game_number": self.game_number
+        }
+
+
+    def get_cars(self):
+        """
+        Make list of car ID's
+        """
+        return self.car_ids
 
 
     def get_moves(self):
+        """
+        Give moves performed in game object
+        """
         return self.moves
+
+
+    def get_move_range(self):
+        """
+        Give move range inherent to board
+        """
+        move_range = list(range(- self.board_size + 2, self.board_size - 1))
+        move_range.remove(0)
+        
+        return move_range
+
 
     def give_board(self):
         """
         Creates and returns board configuration string
         """
-        
         return " ".join(self.board)
 
 
@@ -211,6 +247,6 @@ class Game():
         Checks if the red car (with id X) is in front of the exit
         """
         
-        if self.winning_tile in self.cars['X'].tiles:
+        if self.winning_tile in self.cars['X'].car_attributes()["tiles"]:
             return True
         return False
